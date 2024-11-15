0169cd27c9f2ac0d04de1b69262f9dabf510bdb7
Desmond Roche <desmondroche@gmail.com>
2024-11-15 08:31:00 -0500

    Code added for all examples

--- src/AssignmentOperatorsDemo.java
+++ src/AssignmentOperatorsDemo.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class AssignmentOperatorsDemo {
+
+    public static void main(String[] args) {
+
+            //Assigning primitive values
+        int j, k;
+        j = 10;
+        j = 5;
+        k = j;
+        System.out.println("j is : " + j);
+        System.out.println("k is : " + k);
+
+            //Multiple assignments
+        k = j = 10;
+        System.out.println("j is : " + j);
+        System.out.println("k is : " + k);
+    }
 }
--- src/UnaryOperator.java
+++ src/UnaryOperator.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
 public class UnaryOperator {
+    public static void main(String[] args) {
+
+        int sum = +1; // sum is now 1
+
+        System.out.println(sum);
+
+        sum--; // sum is now 0
+        System.out.println(sum);
+
+        sum++; // sum is now 1
+        System.out.println(sum);
+
+        sum = -sum; // sum is now -1
+        System.out.println(sum);
+
+        boolean result = false;
+
+        System.out.println(result); //false
+        System.out.println(!result); //true
+    }
 }
